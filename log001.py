805a9732bb46f865c90e32dc962243520ec3f61f
MechaOphidian <126343804+MechaOphidian@users.noreply.github.com>
2023-02-25 20:23:51 -0500

    Hello world Exercise
    &print

---
+++ log001.py
@@ -0,0 +1,10 @@
+#log 001
+#Hello World Exercise
+
+print('...Log # 001...')
+
+print('''From the moment I understood the weakness of my flesh,it disgusted me.
+I craved the strength and certainty of steel.
+I aspired to the purity of the blessed machine.
+Your kind cling to your flesh as if it will not decay and fail you.
+One day the crude biomass you call a temple will wither and you will beg my kind to save you.''')
