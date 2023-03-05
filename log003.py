31a360fdde98a7428e5084b8139a5880750fdbc2
MechaOphidian <mechaophidian@gmail.com>
2023-03-04 19:15:34 -0500

    Titles output to CSV

--- log003.py
+++ log003.py
@@ -1,30 +1,33 @@
-#log 003
-#Data scraping a website of its content
+# log 003
+# Data scraping a website of its content
 
 # sourcery skip: merge-dict-assign, use-fstring-for-concatenation, use-fstring-for-formatting
 import requests
 from bs4 import BeautifulSoup
 
-#get a page
-r = requests.get('https://www.sacred-texts.com/')
+# import csv
+import pandas
 
-#parse html
+# get a page
+r = requests.get("https://www.sacred-texts.com/")
+
+# parse html
 soup = BeautifulSoup(r.content, "html.parser")
 
-#print soup.title
+# print soup.title
 print(soup.title)
 
-#prints the title name
+# prints the title name
 print(soup.title.name)
 
-#prints the parent title name
+# prints the parent title name
 print(soup.title.parent.name)
 
 # list of topics from site
 home = soup.find("span", class_="menutext")
 
 # print CSV header
-#print("topic,url")
+# print("topic,url")
 
 topics_raw = []
 
@@ -34,35 +37,47 @@ for elem in home.findChildren("a"):
     href = elem.get("href")
     # get the text of the element
     text = elem.text
-    #create a container for the retrieved url and strip any extra characters off
+    # create a container for the retrieved url and strip any extra characters off
     topic_url = r.url + href.lstrip("./")
-    #Create an empty dictionary
+    # Create an empty dictionary
     temp_dict = {}
-    #sets the "name" key = to the value of text variable
+    # sets the "name" key = to the value of text variable
     temp_dict["name"] = text
-    #sets the "name" key = to the value of topic_url variable
+    # sets the "name" key = to the value of topic_url variable
     temp_dict["url"] = topic_url
-    #adds the keys we just defined to the array
+    # adds the keys we just defined to the array
     topics_raw.append(temp_dict)
 
 
-#Iterate over the items within the array topics_raw and seperate the keys in terminal output
+# Iterate over the items within the array topics_raw and seperate the keys in terminal output
 for topic in topics_raw:
-    #prints a string and concatanates the name key of topic
+    # prints a string and concatanates the name key of topic
     print("Topic: " + topic["name"])
-    #creates an object to hold the result of the request for topic's url
+    # creates an object to hold the result of the request for topic's url
     new_request = requests.get(topic["url"])
-    #if we get a green light from the server then...
+    # if we get a green light from the server then...
     if new_request.status_code == 200:
-        #we put the results of Beautifulsoups .content pull into new_soup using the html.parser function of BS
+        # we put the results of Beautifulsoups .content pull into new_soup using the html.parser function of BS
         new_soup = BeautifulSoup(new_request.content, "html.parser")
-        #if the page has a title element
+        # if the page has a title element
         if new_soup.title is not None:
-            #print the string and concatonate the text of the title element
+            # print the string and concatonate the text of the title element
             print("Page Title: " + new_soup.title.text)
-        #if the page has NO title element
+        # if the page has NO title element
         else:
             print("Page Title not found")
     else:
-        #if we recieve anything but a code 200, print a fail & the url
+        # if we recieve anything but a code 200, print a fail & the url
         print("URL failed " + topic["url"], new_request.status_code)
+
+# Nope
+# with open("topics.csv", "w+", encoding="UTF8") as csvfile:
+#    writer = csv.writer(csvfile)
+#    for topic in topics_raw:
+#        writer.writerow(topic)
+#
+pandasdict = {"name": [topic["name"]], "url": [topic["url"]]}
+
+pandasdataframe = pandas.DataFrame([pandasdict])
+
+pandasdataframe.to_csv("topics.csv")
